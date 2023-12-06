1f9f8ecc3d02ca94344da50633693c12325435d7
Asfakur <saikat.hap@gmail.com>
2023-12-06 18:39:45 +0600

    Countdown added

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import logger from '../services/logService';
+// import { toast } from 'react-toastify';
+// import logger from '../services/logService';
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
 
     if (!expectedError) {
-        logger.log(error);
-        toast.error("An unexpected error occurred", {
-            theme: "dark"
-        })
+        // logger.log(error);
+        // toast.error("An unexpected error occurred", {
+        //     theme: "dark"
+        // })
+        console.log(error);
     }
 
 
